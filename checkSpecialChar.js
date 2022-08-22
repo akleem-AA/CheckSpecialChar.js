3972da7c778e948e5c9752f41bc24823f998a267
akleem <74227145+akleem-AA@users.noreply.github.com>
2022-08-22 13:57:32 +0530

    modify comment

--- checkSpecialChar.js
+++ checkSpecialChar.js
@@ -1,25 +1,23 @@
-function containsSpecialChars(str) {
-  const specialChars = /[`!@#$%^*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+const  containsSpecialChars = (str) => {
+  const specialChars = /[`!@#$&%^*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
   let valid = specialChars.test(str);
-    console.log(valid)
+    console.log("string status = ",valid)
     if(!valid){
       console.log("Valid=",str)
         return str;
     }else{
-        console.log("Not-Valid =",str)
+        console.log("find fist speical char = ",str)
         //divite string into array base on special char
         let checkSpecialChar = str.split(specialChars)
           console.log(checkSpecialChar)
           //getting legth of special char..
           let indexNumber = checkSpecialChar[0].length
-            console.log(indexNumber)
            let result = str.slice(0,indexNumber) + "..."
                 console.log(result)
                 //return final result 
             return result;
     }
-    return valid;
 }
 
-let specialString = containsSpecialChars("Eyewear.& Contacts")
-console.log("return =",specialString)// output Eyewear...
+let specialString = containsSpecialChars(" Beachco&mbing")
+console.log("return =",specialString)
